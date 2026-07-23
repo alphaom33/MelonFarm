deb6beb049ef242b2d1cf254fc6a8d509caa17ad
Dinobird11 <ttrex997@outlook.com>
2026-07-22 23:37:52 -0500

    seccond

--- Assets/WatermelonController.cs
+++ Assets/WatermelonController.cs
@@ -12,6 +12,10 @@ public class WatermelonController : MonoBehaviour
 
     IEnumerator rotator;
     public float rotSpeed;
+    public string WatermelonType;
+
+    //char varient would be first letter of varient, G for gold for example.
+    public char Varient;
 
     private Vector2 lastMove;
     private Rigidbody rb;
@@ -23,52 +27,84 @@ public class WatermelonController : MonoBehaviour
         move = InputSystem.actions.FindAction("Move");
         rb = GetComponent<Rigidbody>();
         targetPosition = transform.position;
+        WatermelonType = "Normal";
     }
 
-    // Update is called once per frame
-    void Update()
+    public void RemoveVarient()
     {
-        Vector2 moveInput = move.ReadValue<Vector2>().normalized;
-        targetPosition = transform.position + moveSpeed * Time.deltaTime * new Vector3(moveInput.x, 0, moveInput.y);
+        //this would run when you are wanting to change varients, before you change the variable "Varient"
+        char Var = Varient;
 
-        if (moveInput != Vector2.zero)
+        switch (Varient)
         {
-            if  (moveInput != lastMove) DoRotate(moveInput);
-        }
-        else if (rotator != null)
-        {
-            StopCoroutine(rotator);
-        }
-        lastMove = moveInput;
-    }
+            case 'G':
+                //reverse effect if varient
+                break;
 
-    void FixedUpdate()
-    {
-        if (!GetComponentInChildren<Caster>().Cast())
-        {
-            rb.MovePosition(targetPosition);
-        }
-        else
-        {
-            Debug.Log("Hit ground");
+
+
         }
     }
-
-    void DoRotate(Vector2 moveInput)
+    public void AddVarient()
     {
-        float target = Mathf.Rad2Deg * Mathf.Atan2(moveInput.y, -moveInput.x) + 90;
+        //this would run when you are wanting to change varients, after you change the variable "Varient"
 
-        if (rotator != null) StopCoroutine(rotator);
-        rotator = DoRotate();
-        StartCoroutine(rotator);
-
-        IEnumerator DoRotate()
+        switch (Varient)
         {
-            while (Mathf.Abs(child.rotation.eulerAngles.y - target) > 0) 
+            case 'G':
+                // add effect of varient
+                break;
+
+        }
+    }
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            Vector2 moveInput = move.ReadValue<Vector2>().normalized;
+            targetPosition = transform.position + moveSpeed * Time.deltaTime * new Vector3(moveInput.x, 0, moveInput.y);
+
+            if (moveInput != Vector2.zero)
             {
-                child.rotation = Quaternion.Euler(0, Mathf.MoveTowardsAngle(child.rotation.eulerAngles.y, target, rotSpeed * Time.deltaTime), 0);
-                yield return new WaitForEndOfFrame();
+                if (moveInput != lastMove) DoRotate(moveInput);
+            }
+            else if (rotator != null)
+            {
+                StopCoroutine(rotator);
+            }
+            lastMove = moveInput;
+
+        }
+
+        void FixedUpdate()
+        {
+            if (!GetComponentInChildren<Caster>().Cast())
+            {
+                rb.MovePosition(targetPosition);
+            }
+            else
+            {
+                Debug.Log("Hit ground");
+            }
+        }
+
+        void DoRotate(Vector2 moveInput)
+        {
+            float target = Mathf.Rad2Deg * Mathf.Atan2(moveInput.y, -moveInput.x) + 90;
+
+            if (rotator != null) StopCoroutine(rotator);
+            rotator = DoRotate();
+            StartCoroutine(rotator);
+
+            IEnumerator DoRotate()
+            {
+                while (Mathf.Abs(child.rotation.eulerAngles.y - target) > 0)
+                {
+                    child.rotation = Quaternion.Euler(0, Mathf.MoveTowardsAngle(child.rotation.eulerAngles.y, target, rotSpeed * Time.deltaTime), 0);
+                    yield return new WaitForEndOfFrame();
+                }
             }
         }
     }
-}
+
