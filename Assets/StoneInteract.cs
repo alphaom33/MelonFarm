ee82e036020fa090e7f1e36fef96cdd277d13059
Dinobird11 <ttrex997@outlook.com>
2026-07-24 01:06:20 -0500

    LETSMINECRAFT

--- Assets/StoneInteract.cs
+++ Assets/StoneInteract.cs
@@ -2,15 +2,33 @@ using UnityEngine;
 
 public class StoneInteract : MonoBehaviour
 {
+    public GameObject StoneDrop;
+    public int hits;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        hits = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    public void Stone()
+    {
+      hits++;
+        if (hits >= 3)
+        {
+            //make stone pickups.
+            Instantiate(StoneDrop, transform.position, Quaternion.identity);
+           
+            Destroy(this.gameObject);
+            Debug.Log("rocking");
+          
+        }
+        
         
     }
 }
--- Assets/TreeInteract.cs
+++ Assets/TreeInteract.cs
@@ -2,15 +2,30 @@ using UnityEngine;
 
 public class TreeInteract : MonoBehaviour
 {
+
+    public GameObject WoodDrop;
+    public int chops;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        chops = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    public void Wood()
+    {
+        chops++;
+        if (chops >= 3)
+        {
+            //make stone pickups.
+            Instantiate(WoodDrop, transform.position, Quaternion.identity);
+            
+            Destroy(this.gameObject);
+            Debug.Log("woody");
+        }
     }
 }
