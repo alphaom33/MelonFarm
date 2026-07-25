e696e7a5aa79a5807eb4dde7e17c9ab544ca9fe4
Dinobird11 <ttrex997@outlook.com>
2026-07-25 15:51:59 -0500

    FixedBug

--- Assets/GrassScript.cs
+++ Assets/GrassScript.cs
@@ -46,6 +46,7 @@ public class GrassScript : MonoBehaviour
                 GameObject backplot = Instantiate(Coin, new Vector3(transform.position.x, transform.position.y + coinoffset, transform.position.z), Quaternion.identity);
                 backplot.GetComponent<CoinScript>().OldPlot = this.gameObject;
             }
+            
         }
 
       
@@ -58,36 +59,48 @@ public class GrassScript : MonoBehaviour
         {
             if (!Righttile.collider.gameObject.CompareTag("Farm"))
             {
-                Debug.Log("Right");
-                GameObject rightplot = Instantiate(Coin, new Vector3(Righttile.transform.position.x, Righttile.transform.position.y + coinoffset, Righttile.transform.position.z), Quaternion.identity);
-                rightplot.GetComponent<CoinScript>().OldPlot = Righttile.collider.gameObject;
+                if (Righttile.collider.gameObject.GetComponent<GrassScript>().Coin == null)
+                {
+                    Debug.Log("Right");
+                    GameObject rightplot = Instantiate(Coin, new Vector3(Righttile.transform.position.x, Righttile.transform.position.y + coinoffset, Righttile.transform.position.z), Quaternion.identity);
+                    rightplot.GetComponent<CoinScript>().OldPlot = Righttile.collider.gameObject;
+                }
             }
         }
         if (Physics.Raycast(transform.position, -transform.right, out RaycastHit Lefttile))
         {
             if (!Lefttile.collider.gameObject.CompareTag("Farm"))
             {
-                Debug.Log("Left");
-                GameObject leftplot = Instantiate(Coin, new Vector3(Lefttile.transform.position.x, Lefttile.transform.position.y + coinoffset, Lefttile.transform.position.z), Quaternion.identity);
-                leftplot.GetComponent<CoinScript>().OldPlot = Lefttile.collider.gameObject;
+                if (Lefttile.collider.gameObject.GetComponent<GrassScript>().Coin == null)
+                {
+                    Debug.Log("Left");
+                    GameObject leftplot = Instantiate(Coin, new Vector3(Lefttile.transform.position.x, Lefttile.transform.position.y + coinoffset, Lefttile.transform.position.z), Quaternion.identity);
+                    leftplot.GetComponent<CoinScript>().OldPlot = Lefttile.collider.gameObject;
+                }
             }
         }
         if (Physics.Raycast(transform.position, transform.forward, out RaycastHit Fronttile))
         {
             if (!Fronttile.collider.gameObject.CompareTag("Farm"))
             {
-                Debug.Log("Forward");
-                GameObject forwardplot = Instantiate(Coin, new Vector3(Fronttile.transform.position.x, Fronttile.transform.position.y + coinoffset, Fronttile.transform.position.z), Quaternion.identity);
-                forwardplot.GetComponent<CoinScript>().OldPlot = Fronttile.collider.gameObject;
+                if (Fronttile.collider.gameObject.GetComponent<GrassScript>().Coin == null)
+                {
+                    Debug.Log("Forward");
+                    GameObject forwardplot = Instantiate(Coin, new Vector3(Fronttile.transform.position.x, Fronttile.transform.position.y + coinoffset, Fronttile.transform.position.z), Quaternion.identity);
+                    forwardplot.GetComponent<CoinScript>().OldPlot = Fronttile.collider.gameObject;
+                }
             }
         }
         if (Physics.Raycast(transform.position, -transform.forward, out RaycastHit Backtile))
         {
             if (!Backtile.collider.gameObject.CompareTag("Farm"))
             {
-                Debug.Log("Backwards");
-                GameObject backplot = Instantiate(Coin, new Vector3(Backtile.transform.position.x, Backtile.transform.position.y + coinoffset, Backtile.transform.position.z), Quaternion.identity);
-                backplot.GetComponent<CoinScript>().OldPlot = Backtile.collider.gameObject;
+                if (Backtile.collider.gameObject.GetComponent<GrassScript>().Coin == null)
+                {
+                    Debug.Log("Backwards");
+                    GameObject backplot = Instantiate(Coin, new Vector3(Backtile.transform.position.x, Backtile.transform.position.y + coinoffset, Backtile.transform.position.z), Quaternion.identity);
+                    backplot.GetComponent<CoinScript>().OldPlot = Backtile.collider.gameObject;
+                }
             }
         }
     }
